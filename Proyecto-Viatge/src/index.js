0e6869445c2a2cf9133ea294c1b2b90fcd81c47f
Luisa <lalarcon@uniempresarial.edu.co>
2024-09-15 23:49:16 -0500

    Api y adelantos
    Api y adelantos

--- Proyecto-Viatge/src/index.js
+++ Proyecto-Viatge/src/index.js
@@ -38,6 +38,8 @@ app.use(personasRoutes);
 //public files
 app.use(express.static(join(__dirname, 'public')));
 
+//----------------------- API ------------------//
+
 // Inicializar el cliente con el token de API
 const client = new ApifyClient({
     token: 'apify_api_zQdcXdiP4QYoOtsicWBOsvNjppmaNw3zk9EW', // Sustituye con tu token de Apify
@@ -45,32 +47,37 @@ const client = new ApifyClient({
 
 
 // Ejemplo de uso de Apify para ejecutar un actor y obtener datos
-async function runApifyTask() {
+async function BookingScraper() {
     try {
-        const actorRun = await client.actor('apify/actor-id').call({
-            // Puedes pasar parámetros aquí según lo que necesites para tu actor
-            input: {
-                destination: 'New York',
-                checkin: '2024-10-01',
-                checkout: '2024-10-05',
-                adults: 2,
-                rooms: 1,
-            },
+        const actorRun = await client.actor('voyager/booking-scraper').call({
+            checkIn: '2024-09-18',
+            checkOut: '2024-10-01',
+            currency: 'COP',
+            language: 'es',
+            maxItems: 5,
+            minMaxPrice: '0-999999',
+            search: 'Colombia',
+            sortBy: 'distance_from_search',
+            starsCountFilter: 'any'
         });
 
-        console.log('Actor run result:', actorRun);
-        
-        // Procesa los resultados del actor
-        const result = await client.dataset(actorRun.defaultDatasetId).listItems();
-        console.log('Scraped data:', result.items);
-        
+        const { items } = await client.dataset(actorRun.defaultDatasetId).listItems();
+        return items;
     } catch (error) {
-        console.error('Error running Apify task:', error);
+        console.error('Error running Booking Scraper:', error);
+        throw error;
     }
 }
 
-// Llamar a la función
-runApifyTask();
+// Configurar la ruta para ejecutar el scraper
+app.get('/run-task', async (req, res) => {
+    try {
+        const scrapedData = await BookingScraper();
+        res.json(scrapedData);
+    } catch (error) {
+        res.status(500).send('Error running task');
+    }
+});
 
 //run server
 app.listen(app.get('port'), ()=>
