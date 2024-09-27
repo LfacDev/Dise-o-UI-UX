3b62cba8e6cf3e4694df186adb8ce9f72b185d13
lizzcaamano <lizethcaama@gmail.com>
2024-09-27 03:30:05 -0500

    mejoras en actividades

--- Proyecto-Viatge/src/routes/actividades.routes.js
+++ Proyecto-Viatge/src/routes/actividades.routes.js
@@ -1,29 +1,12 @@
 import { Router } from "express";
 import pool from '../database.js'
-import { Console } from "console";
 
 const router = Router();
 
 // Ver listado de actividades 
 router.get('/buscar_actividades', async(req, res)=>{
         try{
-            const [result] = await pool.query(`SELECT a.ID_ActividadTuristica AS IDActividad, a.Nombre_Actividad,  a.Descripcion_Actividad, a.Precio_Actividad, t.Nombre_TipoActividadTur, p.Nombre_Pais, i.URL_ImagenActividad,
-                c.Nombre_Ciudad , u.Direccion_Ubicacion, COALESCE(ROUND(AVG(op.Calificacion), 1), 0) AS calificacion, COUNT(op.Comentario) AS Opinion FROM actividad_turistica a 
-                LEFT JOIN ubicacion u ON a.FK_Ubicacion = u.ID_Ubicacion
-                LEFT JOIN tipo_actividadtur t ON a.FK_TipoActividadTur = t.ID_TipoActividadTur
-                LEFT JOIN imagen_actividad i ON a.ID_ActividadTuristica = i.FK_ActividadTuristica
-                LEFT JOIN ciudad c ON u.FK_Ciudad = c.ID_Ciudad
-                INNER JOIN pais p ON p.ID_Pais = c.FK_Pais
-                LEFT JOIN calificaciones op ON op.FK_ActividadTuristica = a.ID_ActividadTuristica 
-                GROUP BY a.ID_ActividadTuristica, 
-                    a.Nombre_Actividad, 
-                    a.Descripcion_Actividad, 
-                    a.Precio_Actividad, 
-                    t.Nombre_TipoActividadTur, 
-                    p.Nombre_Pais, 
-                    i.URL_ImagenActividad, 
-                    c.Nombre_Ciudad, 
-                    u.Direccion_Ubicacion;`);
+            const [result] = await pool.query('call ListarTodasLasActividades()');
             res.render('ActividadesTuristicas/buscar_actividades', {showNav: true, showFooter: true,buscarActividad: result});
             console.log(result);
         }catch(err){
@@ -37,26 +20,17 @@ router.get('/ver_actividad/:IDActividad', async(req, res)=>{
     try {
         const {IDActividad} = req.params;
         const[actividad] = await pool.query(`SELECT 
-    d.FK_ActividadTuristica,
-	a.ID_ActividadTuristica,
-    a.Nombre_Actividad,  
-    a.Descripcion_Actividad, 
-    a.Precio_Actividad, 
-    a.Descripcion_Mini,
-    f.año,
-    f.mes,
-    f.dia,
-    f.hora,
-    d.Disponible,
+    d.FK_ActividadTuristica, d.Disponible, 
+	a.ID_ActividadTuristica, a.Nombre_Actividad, a.Descripcion_Actividad, a.Precio_Actividad, a.Descripcion_Mini, a.EsFavorito,
+    f.año, f.mes, f.dia, f.hora,
     t.Nombre_TipoActividadTur, 
     p.Nombre_Pais, 
     i.URL_ImagenActividad,
     c.Nombre_Ciudad, 
     u.Direccion_Ubicacion, 
-    op.Comentario,
+    op.Comentario, 
     COALESCE(ROUND(AVG(op.Calificacion), 1), 0) AS Calificacion, 
-    COUNT(op.Comentario) AS Cantidad_Opinion,  
-    a.EsFavorito 
+    COUNT(op.Comentario) AS Cantidad_Opinion
     FROM actividad_turistica a 
     LEFT JOIN ubicacion u ON a.FK_Ubicacion = u.ID_Ubicacion
     LEFT JOIN tipo_actividadtur t ON a.FK_TipoActividadTur = t.ID_TipoActividadTur
@@ -68,23 +42,15 @@ router.get('/ver_actividad/:IDActividad', async(req, res)=>{
     LEFT JOIN fechas_disponibles f ON d.FK_FechaDisp = f.ID_FechaDisp
     WHERE a.ID_ActividadTuristica = ?
     GROUP BY 
-    d.FK_ActividadTuristica,
-    a.ID_ActividadTuristica, 
-    a.Nombre_Actividad, 
-    a.Descripcion_Actividad, 
-    a.Precio_Actividad, 
+    d.FK_ActividadTuristica, d.Disponible,
+    a.ID_ActividadTuristica, a.Nombre_Actividad, a.Descripcion_Actividad, a.Precio_Actividad, a.EsFavorito,
     t.Nombre_TipoActividadTur, 
     p.Nombre_Pais, 
     i.URL_ImagenActividad, 
     c.Nombre_Ciudad, 
     u.Direccion_Ubicacion,
-    a.EsFavorito,
-    f.año,
-    f.mes,
-    f.dia,
-    f.hora,
-    op.Comentario,
-    d.Disponible;`,[IDActividad]);
+    f.año, f.mes, f.dia, f.hora,
+    op.Comentario;`,[IDActividad]);
         const verActividad = actividad[0];
         const [fechas] = await pool.query('call VerDispoibilidadActividad(?)', [IDActividad]);    
         res.render('ActividadesTuristicas/ver_actividad', {showNav:true, showFooter:true, actividad: verActividad, fechas: fechas});
